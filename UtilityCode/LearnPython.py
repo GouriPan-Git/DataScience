8b0c30dd063af5b8f2e1551fc6113462eb35409a
GouriPan-Git <88555473+GouriPan-Git@users.noreply.github.com>
2021-09-16 13:12:25 +0530

    Add files via upload

---
+++ UtilityCode/LearnPython.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 12:31:49 2021
+
+@author: gouri
+"""
+
+# This is used to write comments
+
+# Try to print Hello
+
+print("Hello! Python")
+
+# To know the version of python being used
+
+import sys
+print(sys.version)
+
+# Print string and error to see the running order
+
+print("This will be printed")
+#frint("This will cause an error")
+print("This will NOT be printed")
+
+
+# print "Hello, World!"
+
+print("Hello, World!") #Print the traditional Hello World
+
+# Integer
+
+type(10)
+
+# Float
+
+type(4.5)
+
+# String
+
+type("Hello")
+
+
+sys.float_info
+
+
+# Converting Integers to float
+
+type(float(10))
+
+type(float(5.5))
+
+type(int(6.5))
+
+#Converting string to int or float
+type(int('1.2')) #Error invalid literal for int()
+
+type(int('1'))
+
+type(float('1.2'))
+
+type(float('1 0r 2')) #Error could not convert string to float
+
+# Converting interger or float to string
+
+type(str(1))  # output str
+
+str(1) # output '1'
+
+str(1.1) # output '1.1'
+
+#Boolean
+
+type(True)  # bool
+
+type(False)  #bool
+
+bool(1)   # True
+
+bool(0)  # False
+
+float(True)  #1.0
+
+float(False)  #0.0
+
+# Division
+
+6/2  #3.0
+
+6//2 # 3 integer output
+
+
+#How many hours are there in 160 minutes
+
+160//60  # 2 hours
+
+
+
+
+
+
+
+
+
